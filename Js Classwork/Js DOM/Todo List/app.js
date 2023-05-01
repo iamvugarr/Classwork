92c4e34bafd3a980abfdb90f23e4d1b929bc1acb
iamvugarr <ilqar.qurbanov2007@gmail.com>
2023-05-01 23:32:11 +0400

    delete edit button

--- Js Classwork/Js DOM/Todo List/app.js
+++ Js Classwork/Js DOM/Todo List/app.js
@@ -2,9 +2,6 @@ const input = document.getElementById("todo-create");
 const button = document.getElementById("btn");
 const list = document.getElementById("ul");
 
-const editList = document.getElementById("edit-List");
-const editInput = document.getElementById("edit-input");
-const submit = document.getElementById("submit-button");
 
 button.addEventListener("click", () => {
   const liElem = document.createElement("li");
@@ -14,9 +11,7 @@ button.addEventListener("click", () => {
   deleteIcon.className = "fa fa-trash";
   deleteIcon.style.cursor = "pointer";
 
-  editIcon.className = "fas fa-edit";
-  editIcon.style.cursor = "pointer";
-
+  
   if (input.value != "") {
     liElem.innerText = input.value;
     liElem.appendChild(editIcon);
@@ -30,13 +25,11 @@ button.addEventListener("click", () => {
     e.target.parentElement.remove();
     
   });
-
-  editIcon.addEventListener("click", () => {
-    editList.classList.toggle("active");
-    editInput.value = liElem.innerText;
-  });
-
-  submit.addEventListener('click', () => {
-    liElem.innerText = editInput.value
-  })
+  
 });
+
+
+
+
+
+
